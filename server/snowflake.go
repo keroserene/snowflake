da563bc9130e1efddfd313d0cd636eb0b578d4c8
Arlo Breault <arlolra@gmail.com>
2016-01-07 09:26:02 -0800

    Separate client / server

--- server/snowflake.go
+++ server/snowflake.go
@@ -55,11 +55,8 @@ func (c *webRTCConn) Read(b []byte) (int, error) {
 
 func (c *webRTCConn) Write(b []byte) (int, error) {
 	log.Printf("webrtc Write %d %q", len(b), string(b))
-	err := c.dc.Send(b)
-	if err != nil {
-		return 0, err
-	}
-	return len(b), err
+	dc.Send(b)
+	return len(b), nil
 }
 
 func (c *webRTCConn) Close() error {
