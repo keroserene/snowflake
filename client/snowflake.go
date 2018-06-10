keroserene/snowflake

commit 12922a232ba63bd8d94c92ced32e23aa2fb055ed

Make logging optional and controlled by a -log option.

client/snowflake.go
@@ -10,7 +10,6 @@ import (
 	"net"
 	"os"
 	"os/signal"
-	"path"
 	"sync"
 	"syscall"
 	"time"
@@ -122,28 +121,27 @@ func copyLoop(a, b io.ReadWriter) {
 }
 
 func main() {
-	webrtc.SetLoggingVerbosity(1)
-	stateDir, err := pt.MakeStateDir()
-	if err != nil {
-		log.Fatal(err)
-	}
-	logFile, err := os.OpenFile(path.Join(stateDir, "snowflake.log"),
-		os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logFile.Close()
-	log.SetOutput(logFile)
-
 	var iceServers IceServerList
-	log.Println("\n\n\n --- Starting Snowflake Client ---")
-
 	flag.Var(&iceServers, "ice", "comma-separated list of ICE servers")
 	brokerURL := flag.String("url", "", "URL of signaling broker")
 	frontDomain := flag.String("front", "", "front domain")
+	logFilename := flag.String("log", "", "name of log file")
 	max := flag.Int("max", DefaultSnowflakeCapacity,
 		"capacity for number of multiplexed WebRTC peers")
 	flag.Parse()
+
+	webrtc.SetLoggingVerbosity(1)
+	if *logFilename != "" {
+		logFile, err := os.OpenFile(*logFilename,
+			os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer logFile.Close()
+		log.SetOutput(logFile)
+	}
+
+	log.Println("\n\n\n --- Starting Snowflake Client ---")
 
 	// Prepare to collect remote WebRTC peers.
 	snowflakes := NewPeers(*max)

commit 6077141f4affdab9b7ce97a9b1c6859825eaaa29

Log to io.Discard if no log file is set.
https://bugs.torproject.org/25600#comment:14

Snowflake in Tor Browser has been hanging after surfing for a while.
(Tor reports "no running bridges".) It only began happening after commit
12922a232b, which caused snowflake-client not to log to a file by
default (leaving it to log to its default stderr). What seems to be
happening is that tor doesn't read from its PT clients' stderr, leaving
a buffer to fill up that eventually causes a hang.

client/snowflake.go
@@ -150,6 +150,8 @@ func main() {
 		}
 		defer logFile.Close()
 		log.SetOutput(logFile)
+	} else {
+		log.SetOutput(ioutil.Discard)
 	}
 
 	log.Println("\n\n\n --- Starting Snowflake Client ---")