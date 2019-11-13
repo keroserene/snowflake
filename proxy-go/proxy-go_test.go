742070a7fbeef8e8dc0c68060a12266fe60e0ba0
Cecylia Bocovich <cohosh@torproject.org>
2019-11-13 14:31:55 -0500

    Clean up proxy-go tests

--- proxy-go/proxy-go_test.go
+++ proxy-go/proxy-go_test.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -42,14 +43,6 @@ type FaultyTransport struct {
 func (f *FaultyTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return nil, fmt.Errorf("TransportFailed")
 }
-
-const SDP = "v=0\r\no=- 4358805017720277108 2 IN IP4 8.8.8.8\r\ns=-\r\nt=0 0\r\na=group:BUNDLE data\r\na=msid-semantic: WMS\r\nm=application 56688 DTLS/SCTP 5000\r\nc=IN IP4 8.8.8.8\r\na=candidate:3769337065 1 udp 2122260223 8.8.8.8 56688 typ host generation 0 network-id 1 network-cost 50\r\na=candidate:2921887769 1 tcp 1518280447 8.8.8.8 35441 typ host tcptype passive generation 0 network-id 1 network-cost 50\r\na=ice-ufrag:aMAZ\r\na=ice-pwd:jcHb08Jjgrazp2dzjdrvPPvV\r\na=ice-options:trickle\r\na=fingerprint:sha-256 C8:88:EE:B9:E7:02:2E:21:37:ED:7A:D1:EB:2B:A3:15:A2:3B:5B:1C:3D:D4:D5:1F:06:CF:52:40:03:F8:DD:66\r\na=setup:actpass\r\na=mid:data\r\na=sctpmap:5000 webrtc-datachannel 1024\r\n"
-
-const sampleSDP = `"v=0\r\no=- 4358805017720277108 2 IN IP4 8.8.8.8\r\ns=-\r\nt=0 0\r\na=group:BUNDLE data\r\na=msid-semantic: WMS\r\nm=application 56688 DTLS/SCTP 5000\r\nc=IN IP4 8.8.8.8\r\na=candidate:3769337065 1 udp 2122260223 8.8.8.8 56688 typ host generation 0 network-id 1 network-cost 50\r\na=candidate:2921887769 1 tcp 1518280447 8.8.8.8 35441 typ host tcptype passive generation 0 network-id 1 network-cost 50\r\na=ice-ufrag:aMAZ\r\na=ice-pwd:jcHb08Jjgrazp2dzjdrvPPvV\r\na=ice-options:trickle\r\na=fingerprint:sha-256 C8:88:EE:B9:E7:02:2E:21:37:ED:7A:D1:EB:2B:A3:15:A2:3B:5B:1C:3D:D4:D5:1F:06:CF:52:40:03:F8:DD:66\r\na=setup:actpass\r\na=mid:data\r\na=sctpmap:5000 webrtc-datachannel 1024\r\n"`
-
-var sampleOffer = `{"type":"offer","sdp":` + sampleSDP + `}`
-
-const sampleAnswer = `{"type":"answer","sdp":` + sampleSDP + `}`
 
 func TestRemoteIPFromSDP(t *testing.T) {
 	tests := []struct {
@@ -228,6 +221,11 @@ func TestSessionDescriptions(t *testing.T) {
 }
 
 func TestBrokerInteractions(t *testing.T) {
+	const sampleSDP = `"v=0\r\no=- 4358805017720277108 2 IN IP4 8.8.8.8\r\ns=-\r\nt=0 0\r\na=group:BUNDLE data\r\na=msid-semantic: WMS\r\nm=application 56688 DTLS/SCTP 5000\r\nc=IN IP4 8.8.8.8\r\na=candidate:3769337065 1 udp 2122260223 8.8.8.8 56688 typ host generation 0 network-id 1 network-cost 50\r\na=candidate:2921887769 1 tcp 1518280447 8.8.8.8 35441 typ host tcptype passive generation 0 network-id 1 network-cost 50\r\na=ice-ufrag:aMAZ\r\na=ice-pwd:jcHb08Jjgrazp2dzjdrvPPvV\r\na=ice-options:trickle\r\na=fingerprint:sha-256 C8:88:EE:B9:E7:02:2E:21:37:ED:7A:D1:EB:2B:A3:15:A2:3B:5B:1C:3D:D4:D5:1F:06:CF:52:40:03:F8:DD:66\r\na=setup:actpass\r\na=mid:data\r\na=sctpmap:5000 webrtc-datachannel 1024\r\n"`
+
+	const sampleOffer = `{"type":"offer","sdp":` + sampleSDP + `}`
+	const sampleAnswer = `{"type":"answer","sdp":` + sampleSDP + `}`
+
 	Convey("Proxy connections to broker", t, func() {
 		broker := new(Broker)
 		broker.url, _ = url.Parse("localhost")
@@ -257,7 +255,8 @@ func TestBrokerInteractions(t *testing.T) {
 			}
 
 			sdp := broker.pollOffer(sampleOffer)
-			So(sdp.SDP, ShouldEqual, SDP)
+			expectedSDP, _ := strconv.Unquote(sampleSDP)
+			So(sdp.SDP, ShouldResemble, expectedSDP)
 		})
 		Convey("handles poll error", func() {
 			var err error
