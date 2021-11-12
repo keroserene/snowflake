1b79962ca81e6d1db0c790b2d2d5e54a806148f4
Shelikhoo <xiaokangwang@outlook.com>
2021-11-12 10:43:05 +0000

    Rename flag to nat-retest-seconds and retest daily by default
    Adopted the change in according to the recommendation from
    
    https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/-/merge_requests/62#note_2759816
    
    https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/-/merge_requests/62#note_2760512

--- proxy/main.go
+++ proxy/main.go
@@ -18,7 +18,7 @@ func main() {
 	unsafeLogging := flag.Bool("unsafe-logging", false, "prevent logs from being scrubbed")
 	keepLocalAddresses := flag.Bool("keep-local-addresses", false, "keep local LAN address ICE candidates")
 	relayURL := flag.String("relay", sf.DefaultRelayURL, "websocket relay URL")
-	NATTypeMeasurementIntervalSecond := flag.Uint("nat-type-measurement-interval-second", 0,
+	NATTypeMeasurementIntervalSecond := flag.Uint("nat-retest-seconds", 86400,
 		"the time interval in second before NAT type is retested, 0 disables retest")
 
 	flag.Parse()
