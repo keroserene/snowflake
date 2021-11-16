efdb850d2ed7105959a53614b9e881bbee42fd28
Shelikhoo <xiaokangwang@outlook.com>
2021-11-16 11:22:44 +0000

    Update nat-retest-interval flag name to reflect the change
    Adopted the change in according to the recommendation from
    
    https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/-/merge_requests/62#note_2761382

--- proxy/main.go
+++ proxy/main.go
@@ -19,7 +19,7 @@ func main() {
 	unsafeLogging := flag.Bool("unsafe-logging", false, "prevent logs from being scrubbed")
 	keepLocalAddresses := flag.Bool("keep-local-addresses", false, "keep local LAN address ICE candidates")
 	relayURL := flag.String("relay", sf.DefaultRelayURL, "websocket relay URL")
-	NATTypeMeasurementIntervalString := flag.String("nat-retest-seconds", "24h",
+	NATTypeMeasurementIntervalString := flag.String("nat-retest-interval", "24h",
 		"the time interval in second before NAT type is retested, 0s disables retest. Valid time units are \"s\", \"m\", \"h\". ")
 
 	flag.Parse()
