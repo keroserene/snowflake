d2f6ea5417566966bedf8fef261199f0ebc360a2
Shelikhoo <xiaokangwang@outlook.com>
2022-01-06 20:40:21 +0000

    increase clientIDAddrMapCapacity
    See also:
    https://gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/snowflake/-/issues/40084

--- server/lib/http.go
+++ server/lib/http.go
@@ -29,7 +29,7 @@ const clientMapTimeout = 1 * time.Minute
 // How big to make the map of ClientIDs to IP addresses. The map is used in
 // turbotunnelMode to store a reasonable IP address for a client session that
 // may outlive any single WebSocket connection.
-const clientIDAddrMapCapacity = 1024
+const clientIDAddrMapCapacity = 10240
 
 // How long to wait for ListenAndServe or ListenAndServeTLS to return an error
 // before deciding that it's not going to return.
